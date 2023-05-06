3f5db7ebeca6f9c950d0b936a21a78f3acb4e87e
Thegirlwholeaptthroughtime <kdh516094@gmail.com>
2023-05-06 18:31:31 +0900

    0506

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import Counter from "./Counter";
 import Hello from "./Hello";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
 import Wrapper from "./Wrapper";
 import CreateUser from "./CreateUser";
+
+
+function countActiveUsers(users){
+  console.log('활성 사용자 수를 세는중....')
+  return users.filter(user => user.active).length;
+}
+
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -26,21 +33,26 @@ function App() {
         id: 1,
         username: "velopert",
         email: "public.velopert@gmail.com",
+        active: true,
       },
       {
         id: 2,
         username: "tester",
         email: "tester@example.com",
+        active: false,
       },
       {
         id: 3,
         username: "liz",
         email: "liz@example.com",
+        active: false,
       },
     ]
   )
 
   const nextId = useRef(4);
+
+  
   const onCreate = () => {
 
     const user = {
@@ -60,6 +72,18 @@ function App() {
   const onRemove = id => {
     setUsers(users.filter(user => user.id !== id));
   }
+
+ const onToggle = id => {
+  setUsers(
+    users.map(user => 
+        user.id === id ? {...user, active : !user.active} : user
+      )
+  )  
+}
+
+/* const count = countActiveUsers(users) */
+ const count = useMemo(() => countActiveUsers(users), [users]) 
+//memo : memorized 를 의미함. 이는 이전에 계산한값을 재사용한다는 의미..
   return (
     <Wrapper>
       {/*  <Hello name="react" color="red" isSpecial={true}/>
@@ -77,8 +101,13 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
-      <InputSample />
+      <UserList 
+        users={users} 
+        onRemove={onRemove} 
+        onToggle={onToggle}  
+      />
+      <div>활성사용자 수 : {count}</div>
+      {/* <InputSample /> */}
     </Wrapper>
   );
 }
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function User({ user,onRemove }) {
+function User({ user, onRemove, onToggle }) {
+
+
+    /*   
+    useEffect(() => {
+        console.log('user값이 설정됨')
+        console.log(user)
+        return () => {
+            console.log('user가 바뀌기 전..')
+            console.log(user)
+        }
+    }, [user]);
+
+    마운트 : 처음 나타남
+    언마운트 : 사라짐
+    useEffect : 함수 
+    첫번째인자는 함수 / 두번째 인자는 배열(Deps라 명칭)
+    */
+  
+
+    useEffect(() => {
+        //console.log(user);
+    })
+    // 컴포넌트가 리렌더링 될때마다
     return (
         <div>
-            <b>ID : {user.username}</b>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                ID : {user.username}</b>
             <div>Email :<span>{user.email}</span></div>
             <button onClick={() => onRemove(user.id)}>삭제</button>
             <span>----------------------</span>
@@ -13,13 +43,18 @@ function User({ user,onRemove }) {
 }
 
 
-function UserList({users,onRemove}) {
+function UserList({ users, onRemove, onToggle }) {
 
- 
+
     return (
         <div>
             {users.map(user => (
-                <User key={user.id} user={user} onRemove={onRemove} />
+                <User
+                    key={user.id}
+                    user={user}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );
